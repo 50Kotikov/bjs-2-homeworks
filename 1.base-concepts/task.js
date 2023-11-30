f772041b74b5956d20bf1805c98f06dd9fe87749
50_Kotikov <50malvinok@gmail.com>
2023-11-30 22:19:05 +0300

    change task 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,13 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let discriminant = (b ** 2) - (4 * a * c);
+  let d = (b ** 2) - (4 * a * c);
   let corn1;
   let corn2;
-  if(discriminant === 0) {
-    corn = - b / (2 * a);
-    arr.push(corn);
-  } else if(discriminant > 0) {
+  if(d === 0) {
+    corn1 = - b / (2 * a);
+    arr.push(corn1);
+  } else if(d > 0) {
     corn1 = ((- b + Math.sqrt(d)) / (2 * a));
     corn2 = ((-b - Math.sqrt(d)) / (2 * a));
     arr.push(corn1, corn2);
@@ -25,9 +25,10 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let percentDiapoz = 0.01;
   let p = (percent * percentDiapoz) / months;
   let s = amount - contribution;
-  let everyMonthpayment = s * (p + (p / (((1 + p) ** n) - 1)));
-  let creditSumm = Number(everyMonthpayment * countMonths).toFixed(2);
+  let n = countMonths;
+  let everyMonthPayment = s * (p + (p / (((1 + p) ** n) - 1)));
+  let creditSumm = Number(everyMonthPayment * countMonths).toFixed(2);
   
-  return creditSumm;
+  return Number(creditSumm);
 
 }
