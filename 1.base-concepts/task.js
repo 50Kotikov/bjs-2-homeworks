bd4d12b975ade614f3735ae826d40782d34bc9ae
50_Kotikov <50malvinok@gmail.com>
2023-11-30 22:09:32 +0300

    Task 1 and task 2 done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = (b ** 2) - (4 * a * c);
+  let corn1;
+  let corn2;
+  if(discriminant === 0) {
+    corn = - b / (2 * a);
+    arr.push(corn);
+  } else if(discriminant > 0) {
+    corn1 = ((- b + Math.sqrt(d)) / (2 * a));
+    corn2 = ((-b - Math.sqrt(d)) / (2 * a));
+    arr.push(corn1, corn2);
+  } else {
+     arr = [];
+  };
+
   return arr;
 }
+console.log(solveEquation());
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let months = 12;
+  let percentDiapoz = 0.01;
+  let p = (percent * percentDiapoz) / months;
+  let s = amount - contribution;
+  let everyMonthpayment = s * (p + (p / (((1 + p) ** n) - 1)));
+  let creditSumm = Number(everyMonthpayment * countMonths).toFixed(2);
   
+  return creditSumm;
+
 }
