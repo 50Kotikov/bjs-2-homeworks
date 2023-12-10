ce71434d5daba8b58d8db614afbc233cb00268b2
50_Kotikov <50malvinok@gmail.com>
2023-12-10 14:48:11 +0300

    task 4 and 5 done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -16,14 +16,25 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-   Student.hasOwnProperty(marks);
-   this.marks.push(...marks);
+   if (this.hasOwnProperty('marks') === true) {
+   this.marks.push(...marks)
+   };
 }
 
 Student.prototype.getAverage = function () {
+    let avaregeMarks;
+    if (this.hasOwnProperty('marks') === false || this.marks.length === 0) {
+        avaregeMarks = 0}
+        else {let sumMarks = this.marks.reduce((acc, number) => acc + number, 0);
+            avaregeMarks = sumMarks / this.marks.length;
+     }; 
+    return avaregeMarks;  
   
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason; 
+
 }
