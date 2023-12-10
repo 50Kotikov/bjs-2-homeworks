dadc7977a4b108d850210662e433aa8777d54f9b
50_Kotikov <50malvinok@gmail.com>
2023-12-10 13:40:02 +0300

    change task 1, 2 and 3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,21 +1,23 @@
 function Student(name, gender, age) {
-    return {
-        name,
-        gender,
-        age,
-        marks: [],
-    };  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+    return; 
+}; 
+
 
 new Student();
 
 
 Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
   
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+   Student.hasOwnProperty(marks);
+   this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
