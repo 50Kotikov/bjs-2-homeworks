75687336e321145666e5c11324a08452b27d958e
50_Kotikov <50malvinok@gmail.com>
2024-01-02 20:56:07 +0300

    task 1 done

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,16 @@
-﻿
+﻿function parseCount(number){
+    let findNumber = parseFloat(number);
+    if(Number.isNaN(findNumber) === true){
+        throw new Error("Невалидное значение");
+    }
+    return findNumber;
+};
+
+function validateCount(number){
+    try{
+        return parseCount(number);
+    }catch(error){
+        return error
+    };
+};
+
